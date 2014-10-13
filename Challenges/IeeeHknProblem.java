67ceabeaa8e1e64adcd916cbb1c8758232a63e42
Erkanamen <timote.vaucher@epfl.ch>
2014-10-13 21:54:03 +0200

    Version plus optimisée de Hkn Problem

--- Challenges/IeeeHknProblem.java
+++ Challenges/IeeeHknProblem.java
@@ -1,14 +1,9 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
 public class IeeeHknProblem {
 	private static Scanner clavier = new Scanner(System.in);
-    
-	private static boolean isBitPalindrome(int x) {
-	    int reversed = 0, aux = x;
+	private static boolean isBitPalindrome(long x) {
+	    long reversed = 0, aux = x;
 	    while (aux > 0) {
 	    	reversed = (reversed  << 1) | (aux & 1);
 	    	aux = aux >> 1;
@@ -17,38 +12,31 @@ public class IeeeHknProblem {
 	}
 	public static void main(String[] args) {
 		//Input
-		String input=clavier.nextLine(), inputCleared="";
-		for (int i=0; i<input.length();++i) {
-			Character c = input.charAt(i);
-			//System.out.print(c + " ");
-			if (Pattern.matches("[0-9,]",c.toString())) {
-				inputCleared+=c.toString();
-			}
-		}
-		//System.out.println("a:" + inputCleared);
-		String[] inputLines = inputCleared.split(",{1,}"); //Split the lines
+		String input=clavier.nextLine();
+		String[] inputLines = input.split(",{1,}"); //Split the lines
 		if (inputLines.length != 2) {
 			System.exit(0);
 		}
-		else
-		{
-			int a = -1, b = -1;
-			try {
-				a=Integer.parseInt(inputLines[0]);
-				b=Integer.parseInt(inputLines[1]);
-			}
-			catch (Exception e) {
-				System.exit(0);
-			}
-			if (a>b || a<0 || b>(int) Math.pow(2, 32)-1) {
+		else {
+			long a = Long.parseLong(inputLines[0]), b=Long.parseLong(inputLines[1]);
+			if (a>b || a<0 || b>(long) Math.pow(2, 32)-1) {
 				System.exit(0);
 			}
 			else {
 				int compteur = 0;
-				for (int i=a; i<=b; ++i) {
+				long i=a;
+				if (i==0) {
+					++i;
+					++compteur;
+				}
+				else if (i%2==0){ //un nombre pair ne peut pas être palindrome
+					++i;
+				}
+				while(i<=b) {
 					if (isBitPalindrome(i)) {
 						++compteur;
 					}
+					i+=2;
 				}
 				System.out.println(compteur);
 			}
