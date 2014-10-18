7a0443f39f486d4b90541e570bf595ef7a7c20cd
Maxime Kjaer <maxime.kjaer@gmail.com>
2014-10-18 03:37:20 +0200

    v2 Challenge 1
    Reduce the sorting.
    Still fails the same tests.

--- Challenges/Challenge 1/K-value.py
+++ Challenges/Challenge 1/K-value.py
@@ -1,6 +1,6 @@
 while True:
 	n, m, k = list(map(int, input().strip().split(' ')))
-	integers = list(map(int, input().strip.split(' ')[:int(n)]))
+	integers = list(map(int, input().strip().split(' ')[:int(n)]))
 	condition1 = (1 < n and n <= 100000)
 	condition2 = (1 <= m and m < n)
 	condition3 = (1 <= k and k <= m)
@@ -12,9 +12,13 @@ while True:
 	if (condition1 and condition2 and condition3 and condition4):
 		break
 
-k_smallest = []
+sublist = integers[:m]
+k_smallest = sorted(sublist)[k-1]
 
-for i in range(0, n):
-	sublist = sorted([integers[i%n] for i in range(i, i+m)])
-	k_smallest.append(sublist[k-1])
-print(min(k_smallest))
+for i in range(m, n + m - 1):
+	sublist = sublist[1:] + [integers[i % n]]
+	list_k_smallest = sorted(sublist)[k-1]
+	if list_k_smallest < k_smallest:
+		k_smallest = list_k_smallest
+
+print(k_smallest)
