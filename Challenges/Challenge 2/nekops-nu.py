803d5f6ca0e1129f83baf2cf7505e0b83f842d15
Maxime Kjaer <maxime.kjaer@gmail.com>
2014-10-18 08:42:10 +0200

    Update, +10 points

--- Challenges/Challenge 2/nekops-nu.py
+++ Challenges/Challenge 2/nekops-nu.py
@@ -17,6 +17,7 @@ def nekops_nu(sequence):
 	return result
 
 max_length = len(seq)
+max_seq = seq
 seq_of_seq.append(seq)
 
 for _ in range(k):
@@ -24,8 +25,17 @@ for _ in range(k):
 	seq_of_seq.append(seq)
 	if max_length < len(seq):
 		max_length = len(seq)
+		max_seq = seq
 
+max_string_length = len(' '.join(list(map(str, max_seq))))
 for step in seq_of_seq:
-	dots = (max_length - len(step))
-	print('.'*dots + ' '.join(list(map(str, step))) + '.'*dots)
+
+	string = ' '.join(list(map(str, step)))
+	dots = (max_string_length - len(string)) // 2
+	string = '.'*dots + string + '.'*dots
+	
+	if len(string) == max_string_length - 1:
+		string = '.' + string
+	print(string)
+
 print(len(seq))
