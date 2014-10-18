62ca13ac42bdc59b67d2c853470d8467365d79e9
Maxime Kjaer <maxime.kjaer@gmail.com>
2014-10-18 03:43:00 +0200

    v3 Challenge 1
    50 points - fails on 2, 6, 7, 8, 9

--- Challenges/Challenge 1/K-value.py
+++ Challenges/Challenge 1/K-value.py
@@ -1,16 +1,7 @@
-while True:
-	n, m, k = list(map(int, input().strip().split(' ')))
-	integers = list(map(int, input().strip().split(' ')[:int(n)]))
-	condition1 = (1 < n and n <= 100000)
-	condition2 = (1 <= m and m < n)
-	condition3 = (1 <= k and k <= m)
-	condition4 = True
-	for val in integers:
-		condition4 = (0 <= val and val <= 2147483647)
-		if not condition4:
-			break
-	if (condition1 and condition2 and condition3 and condition4):
-		break
+n, m, k = list(map(int, input().strip().split(' ')))
+integers = list(map(int, input().strip().split(' ')[:int(n)]))
+
+minimum = min(integers)
 
 sublist = integers[:m]
 k_smallest = sorted(sublist)[k-1]
@@ -18,7 +9,10 @@ k_smallest = sorted(sublist)[k-1]
 for i in range(m, n + m - 1):
 	sublist = sublist[1:] + [integers[i % n]]
 	list_k_smallest = sorted(sublist)[k-1]
-	if list_k_smallest < k_smallest:
+	if list_k_smallest == minimum:
+		k_smallest = minimum
+		break
+	elif list_k_smallest < k_smallest:
 		k_smallest = list_k_smallest
 
 print(k_smallest)
